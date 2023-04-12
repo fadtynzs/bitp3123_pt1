055251c05dbcb9b45da73bd156346ddefe79df8e
fadtynzs <b032120072@student.utem.edu.my>
2023-04-12 17:27:36 +0800

    initial commit

--- samples/src/samples/GitTest.java
+++ samples/src/samples/GitTest.java
@@ -3,7 +3,7 @@ package samples;
 public class GitTest {
 	public static void main(String[] args) {
         System.out.println("Git Is Fun");
-        System.out.println("Git Is ");
+        System.out.println("Git Is cooljj");
         
 	}
 }
